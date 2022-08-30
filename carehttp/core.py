d4474f4ee8d0ccd683ff30885b3b46c0722025a0
pgshow <pgshow@gmail.com>
2022-08-30 20:06:22 +0800

    add explain for wait_exponential_max and self.delay

--- carehttp/core.py
+++ carehttp/core.py
@@ -45,8 +45,8 @@ class Carehttp:
         # Decorate functions to be retried
         retry_decorator = retry(
             stop_max_attempt_number=self.tries,  # retry times
-            wait_exponential_multiplier=self.delay,
-            wait_exponential_max=self.max_delay,  # Wait 2^x * 1000 milliseconds between each retry
+            wait_exponential_multiplier=self.delay,  # Wait 2^x * 1000 milliseconds between each retry
+            wait_exponential_max=self.max_delay,
             retry_on_exception=lambda exc: _retry_if_err(exc, self),
         )
 
